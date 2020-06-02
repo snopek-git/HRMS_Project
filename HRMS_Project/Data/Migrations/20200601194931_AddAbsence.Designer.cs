e0876945cb0ac2840acce7fe5c946449a36c725d
marek <marek.snopek@yahoo.com>
2020-06-02 19:47:42 +0200

    AbsenceModel

--- HRMS_Project/Data/Migrations/20200601194931_AddAbsence.Designer.cs
+++ HRMS_Project/Data/Migrations/20200601194931_AddAbsence.Designer.cs
@@ -4,14 +4,16 @@ using HRMS_Project.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HRMS_Project.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200601194931_AddAbsence")]
+    partial class AddAbsence
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HRMS_Project/Models/AbsenceType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMS_Project.Models
+{
+    public class AbsenceType
+    {
+        public AbsenceType()
+        {
+            AvailableAbsence = new HashSet<AvailableAbsence>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdAbsenceType { get; set; }
+        public string AbsenceTypeName { get; set; }
+
+        public virtual ICollection<AvailableAbsence> AvailableAbsence { get; set; }
+    }
+}
